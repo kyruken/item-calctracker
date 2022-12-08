22474f79071344825ef973730451ae9afbeb4a31
Avery Kyle Arenas <kyruken@gmail.com>
2022-12-07 18:33:06 -0800

    create create receipt logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
+
+import Item from './components/item';
 function App() {
   
   const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState('');
-  const [itemCost, setItemCost] = useState();
-  
+  const [itemCost, setItemCost] = useState('');
+  const [totalCost, setTotalCost] = useState();
+
   function clickHandler(event) {
     event.preventDefault();
     setItems(prevItems => [...prevItems, {name: itemName, cost: itemCost}])
+  }
+  
+  function submitItems() {
+    let total = 0;
+    for (let x = 0; x < items.length; x++) {
+        total += parseInt(items[x].cost);
+    }
+    total += total * .925;
+    setTotalCost(total);
   }
   
   function onChangeNameHandler(event) {
@@ -18,6 +30,13 @@ function App() {
     setItemCost(event.target.value);
   }
 
+  const itemElements = items.map(item => {
+    return <Item 
+        name={item.name}
+        cost={item.cost}
+    />
+  })
+
 
   return (
     <div className="App">
@@ -25,8 +44,19 @@ function App() {
       <form>
         <input type='text' placeholder='Name of item' name='name' onChange={(e) => onChangeNameHandler(e)}></input>
         <input type='text' placeholder='cost' name='cost' onChange={(e) => onChangeCostHandler(e)}></input>
-        <button onClick={(event) => clickHandler(event)}>Submit</button>
+        <button onClick={(event) => clickHandler(event)}>Add Item</button>
       </form>
+      {totalCost && 
+      <button>Reset</button>
+      }
+      <button onClick={submitItems}>Create receipt</button>
+      {itemElements}
+      {totalCost && 
+      <div> 
+        <p>Item tax: 9.25%</p>
+        <h2>Total cost is ${totalCost}</h2>
+      </div>
+      }
     </div>
   )
 }
